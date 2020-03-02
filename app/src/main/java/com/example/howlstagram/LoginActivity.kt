b25329e46a5f7d601eac2e619d1e022c2d09bafd
linear14 <47495592+linear14@users.noreply.github.com>
2020-03-02 18:51:24 +0900

    Login 화면 구성 : TextInputLayout / TextInputEditText 공부.

---
+++ app/src/main/java/com/example/howlstagram/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.howlstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// android textInputLayout -> material library 사용
+// https://prince-mint.tistory.com/7 여기에 약간의 정보 있음
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
