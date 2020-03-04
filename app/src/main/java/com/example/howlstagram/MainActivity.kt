733c7613e37ffd0cc9ca6773167731e94b4be9c6
linear14 <47495592+linear14@users.noreply.github.com>
2020-03-04 12:04:43 +0900

    5차시 - toolbar, bottomNavigation, Fragment 기본 메인 화면 구성. 메인 화면을 toolbar, frame(content), bottomNavigation 으로 구분 toolbar에 이미지 하나 넣는 것 구현 bottomNavigation에 menu등록.(핵심) 네비게이션 아이템을 클릭할 때 마다 fragment로 화면 전환 (navigation 폴더를 만들어서 따로 관리) onNavigationItemSelectedListener 인터페이스로 사용. 이런식으로 리스너 사용하는 것을 앞으로 연습

--- app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -2,11 +2,53 @@ package com.example.howlstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.example.howlstagram.navigation.AlarmFragment
+import com.example.howlstagram.navigation.DetailViewFragment
+import com.example.howlstagram.navigation.GridFragment
+import com.example.howlstagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+
     }
+
+    // 리스너 이렇게 사용하는게 더 깔끔한 것 같음. onCreate가 일단 깔끔해지고.. 재활용성도 있고
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
 }
